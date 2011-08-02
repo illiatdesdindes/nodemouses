67dc6c579d80f8fd9413090640eb2a5740aee99b
illiatdesdindes <illiatdesdindes@gmail.com>
2011-08-02 10:59:40 +0200

    Display user's name next to his mouse

--- client.js
+++ client.js
@@ -1,3 +1,6 @@
+
+
+
 function ratelimit(fn, ms) {
   var last = (new Date()).getTime();
   return (function() {
@@ -13,9 +16,9 @@ function removeMouse(id) {
   $('#'+id).remove();
 }
 
-function moveMouse(id, x, y) {
+function moveMouse(id, x, y, nom) {
   if ( $('#'+id).size() == 0){
-    $('body').append("<img id='"+id+"' class='mouse' src='http://ami71.no-ip.org/pointer.png' />");
+    $('body').append("<div id='"+id+"' class='mouse'><img src='/nodemouses/pointer.png' />"+nom+"</div>");
   }
   $('#'+id).css({
     'left' : x + 'px',
@@ -26,7 +29,7 @@ function moveMouse(id, x, y) {
 function hub(msg){
   switch (msg[0]) {
     case 'coord': // "coord:id:x:y"
-        moveMouse(msg[1], msg[2], msg[3])
+        moveMouse(msg[1], msg[2], msg[3], msg[4])
       break;
       
     case 'disconnect': // "disconnect:id"
@@ -35,7 +38,9 @@ function hub(msg){
   }
 }
 
-var socket = new io.Socket("ami71.no-ip.org",{port: 8080}); 
+var nom = prompt("Votre nom svp:");
+
+var socket = new io.Socket(location.hostname,{port: 8080}); 
 socket.connect();
 
 socket.on('connect', function(){ 
@@ -48,10 +53,13 @@ socket.on('message', function(d){
   hub(msg)
 }); 
 
-socket.on('disconnect', function(){}); 
+socket.on('disconnect', function(){});
+
 
 $(document).mousemove( 
   ratelimit( function(e){
-    socket.send(e.pageX +":"+e.pageY)
+    socket.send(e.pageX +":"+e.pageY+":"+nom)
     }, 100)
 );
+
+
